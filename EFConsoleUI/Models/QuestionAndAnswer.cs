88f3e1827d257254acadc590c5003ee95818b951
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-07-27 15:19:45 +0300

    Created model for the data table, added nugets (EF and JSON), added appsettings.json with connection string.

---
+++ EFConsoleUI/Models/QuestionAndAnswer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EFConsoleUI.Models
+{
+    public class QuestionAndAnswer
+    {
+        public int Id { get; set; }
+        public int ShowNumber { get; set; }
+        public DateTime AirDate { get; set; }
+        public string Round { get; set; }
+        public string Category { get; set; }
+        public int Value { get; set; }
+        public string Currency { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
---
+++ EFConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
