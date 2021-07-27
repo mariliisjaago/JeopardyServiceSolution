a7e28c98104b4a680ce9090d027283020b2e8865
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-07-27 23:13:05 +0300

    Migration script to create database. Added connection string data to gitignore file.

---
+++ DataAccessLibrary/Migrations/20210727200403_CreateDatabase.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLibrary.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rounds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoundName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rounds", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shows",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShowNumber = table.Column<int>(type: "int", nullable: false),
+                    AirDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shows", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuestionsAndAnswers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShowDataId = table.Column<int>(type: "int", nullable: true),
+                    RoundId = table.Column<int>(type: "int", nullable: true),
+                    CategoryId = table.Column<int>(type: "int", nullable: true),
+                    Value = table.Column<int>(type: "int", nullable: false),
+                    Question = table.Column<string>(type: "nvarchar(900)", maxLength: 900, nullable: false),
+                    Answer = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionsAndAnswers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuestionsAndAnswers_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_QuestionsAndAnswers_Rounds_RoundId",
+                        column: x => x.RoundId,
+                        principalTable: "Rounds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_QuestionsAndAnswers_Shows_ShowDataId",
+                        column: x => x.ShowDataId,
+                        principalTable: "Shows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionsAndAnswers_CategoryId",
+                table: "QuestionsAndAnswers",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionsAndAnswers_RoundId",
+                table: "QuestionsAndAnswers",
+                column: "RoundId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionsAndAnswers_ShowDataId",
+                table: "QuestionsAndAnswers",
+                column: "ShowDataId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionsAndAnswers_Value",
+                table: "QuestionsAndAnswers",
+                column: "Value");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shows_AirDate",
+                table: "Shows",
+                column: "AirDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuestionsAndAnswers");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Rounds");
+
+            migrationBuilder.DropTable(
+                name: "Shows");
+        }
+    }
+}
