cbd3317bde56024a7891c8a0409b9edddfc0ca6a
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-07-27 21:51:09 +0300

    Added decorators to models.

--- DataAccessLibrary.Models/QuestionAndAnswer.cs
+++ DataAccessLibrary.Models/QuestionAndAnswer.cs
@@ -1,14 +1,26 @@
-﻿namespace DataAccessLibrary.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLibrary.Models
 {
     public class QuestionAndAnswer
     {
         public int Id { get; set; }
+        [Required]
         public int ShowNumber { get; set; }
+        [Required]
         public string Round { get; set; }
+        [Required]
+        [MaxLength(150)]
         public string Category { get; set; }
+        [Required]
         public int Value { get; set; }
+        [Required]
         public string Currency { get; set; }
+        [Required]
+        [MaxLength(900)]
         public string Question { get; set; }
+        [Required]
+        [MaxLength(150)]
         public string Answer { get; set; }
     }
 }
--- DataAccessLibrary.Models/Show.cs
+++ DataAccessLibrary.Models/Show.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataAccessLibrary.Models
 {
     public class Show
     {
         public int Id { get; set; }
+        [Required]
         public int ShowNumber { get; set; }
+        [Required]
         public DateTime AirDate { get; set; }
     }
 }
