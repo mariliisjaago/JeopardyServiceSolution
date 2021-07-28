8866d4e44a9d2bd541cfba738fbfc9fe82aa6742
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-07-28 11:38:46 +0300

    Added models to hold raw input data. Added first lists to hold the models.

---
+++ DataAccessLibrary.Models/RawShowModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DataAccessLibrary.Models
+{
+    public class RawShowModel
+    {
+        public int ShowNumber { get; set; }
+        public DateTime AirDate { get; set; }
+    }
+}
--- DataAccessLibrary/Migrations/20210728080913_PopulateDatabase.cs
+++ DataAccessLibrary/Migrations/20210728080913_PopulateDatabase.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using DataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Collections.Generic;
 
 namespace DataAccessLibrary.Migrations
 {
     public partial class PopulateDatabase : Migration
     {
+        private List<RawQuestionModel> questions = new List<RawQuestionModel>();
+
+        private List<RawShowModel> shows = new List<RawShowModel>();
+
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             // read in the csv files
+
+
 
             // go over Shows file, put to table
 
@@ -23,7 +31,6 @@ namespace DataAccessLibrary.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            // truncate tables to empties
             migrationBuilder.Sql("DELETE FROM dbo.QuestionsAndAnswers;");
 
             migrationBuilder.Sql("DELETE FROM dbo.Shows;");
