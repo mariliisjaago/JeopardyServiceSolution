2e63dffe29d6c548d9317c5fea4179fa5d517e9a
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-08-01 21:07:04 +0300

    Added try-catches to both ApiControllers.

--- JeopardyApi/Controllers/KeywordController.cs
+++ JeopardyApi/Controllers/KeywordController.cs
@@ -22,13 +22,27 @@ namespace JeopardyApi.Controllers
         [ProducesResponseType(404)]
         public IActionResult GetByKeyword(string keyword)
         {
-            var results = _dbContext.QuestionsAndAnswers
+            try
+            {
+                var results = _dbContext.QuestionsAndAnswers
                 .Where(x => x.Question.Contains(keyword) | x.Answer.Contains(keyword))
                 .Include(x => x.ShowData)
                 .Include(x => x.Round)
                 .Include(x => x.Category);
 
-            return Ok(results);
+                if (results == null)
+                {
+                    return Ok("Your keyword was not found in given Jeopardy questions nor answers.");
+                }
+                else
+                {
+                    return Ok(results);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Something went wrong");
+            }
         }
     }
 }
--- JeopardyApi/Controllers/ValueDateController.cs
+++ JeopardyApi/Controllers/ValueDateController.cs
@@ -31,32 +31,39 @@ namespace JeopardyApi.Controllers
         {
             IOrderedQueryable<ValueDateResultModel> intermediateData;
 
-            if (lookupModel.IsFinalQuestion == true)
+            try
             {
+                if (lookupModel.IsFinalQuestion == true)
+                {
+                    intermediateData = _dbContext.QuestionsAndAnswers
+                        .Where(x => x.Round.RoundName == "Final Jeopardy!")
+                        .Where(x => x.ShowData.AirDate.Month == lookupModel.Date.Month && x.ShowData.AirDate.Year == lookupModel.Date.Year)
+                        .Include(x => x.Round)
+                        .Include(x => x.Category)
+                        .Include(x => x.ShowData)
+                        .Select(x => new ValueDateResultModel { DifferenceInDays = Math.Abs(EF.Functions.DateDiffDay(x.ShowData.AirDate, lookupModel.Date)), QuestionData = x })
+                        .OrderBy(x => x.DifferenceInDays);
+                }
+
                 intermediateData = _dbContext.QuestionsAndAnswers
-                    .Where(x => x.Round.RoundName == "Final Jeopardy!")
+                    .Where(x => x.Value == lookupModel.Value)
                     .Where(x => x.ShowData.AirDate.Month == lookupModel.Date.Month && x.ShowData.AirDate.Year == lookupModel.Date.Year)
                     .Include(x => x.Round)
                     .Include(x => x.Category)
                     .Include(x => x.ShowData)
                     .Select(x => new ValueDateResultModel { DifferenceInDays = Math.Abs(EF.Functions.DateDiffDay(x.ShowData.AirDate, lookupModel.Date)), QuestionData = x })
                     .OrderBy(x => x.DifferenceInDays);
+
+                DateTime closestShowDate = intermediateData.FirstOrDefault().QuestionData.ShowData.AirDate;
+
+                List<QuestionAndAnswer> results = intermediateData.Where(x => x.QuestionData.ShowData.AirDate == closestShowDate).Select(x => x.QuestionData).ToList();
+
+                return Ok(results);
             }
-
-            intermediateData = _dbContext.QuestionsAndAnswers
-                .Where(x => x.Value == lookupModel.Value)
-                .Where(x => x.ShowData.AirDate.Month == lookupModel.Date.Month && x.ShowData.AirDate.Year == lookupModel.Date.Year)
-                .Include(x => x.Round)
-                .Include(x => x.Category)
-                .Include(x => x.ShowData)
-                .Select(x => new ValueDateResultModel { DifferenceInDays = Math.Abs(EF.Functions.DateDiffDay(x.ShowData.AirDate, lookupModel.Date)), QuestionData = x })
-                .OrderBy(x => x.DifferenceInDays);
-
-            DateTime closestShowDate = intermediateData.FirstOrDefault().QuestionData.ShowData.AirDate;
-
-            List<QuestionAndAnswer> results = intermediateData.Where(x => x.QuestionData.ShowData.AirDate == closestShowDate).Select(x => x.QuestionData).ToList();
-
-            return Ok(results);
+            catch (Exception)
+            {
+                return BadRequest("Something went wrong");
+            }
         }
     }
 }
